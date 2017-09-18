a959937d5c2368d6d1f645a551da3d9dc309dd0a
Rodionov Aleksandr <allrod@mail.ru>
2017-09-18 09:47:26 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+[![Build Status](https://travis-ci.org/rodionovsasha/CommentService.svg?branch=master)](https://travis-ci.org/rodionovsasha/CommentService)
+
+# Comment Service
+Demo project to show how JFixtures works on practice  
+https://github.com/vkorobkov/jfixtures
+
+## Rest JSON API
+<pre>
+http://localhost:8080/v1/api
+</pre>
---
+++ src/test/resources/schema.sql
@@ -0,0 +1,24 @@
+CREATE TABLE IF NOT EXISTS user (
+  id int PRIMARY KEY AUTO_INCREMENT,
+  name varchar(255),
+  age int,
+  enabled boolean default true
+);
+
+CREATE TABLE IF NOT EXISTS topic (
+  id int PRIMARY KEY AUTO_INCREMENT,
+  user_id int,
+  title varchar(255),
+  date timestamp default CURRENT_TIMESTAMP(),
+  foreign key (user_id) references user(id)
+);
+
+CREATE TABLE IF NOT EXISTS comment (
+  id int PRIMARY KEY AUTO_INCREMENT,
+  user_id int,
+  topic_id int,
+  content text,
+  date timestamp default CURRENT_TIMESTAMP(),
+  foreign key (user_id) references user(id),
+  foreign key (topic_id) references topic(id)
+);
