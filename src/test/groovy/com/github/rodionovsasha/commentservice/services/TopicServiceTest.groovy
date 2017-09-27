b1718e092ff069ecad672c6561214f8da103bfe2
Rodionov Aleksandr <allrod@mail.ru>
2017-09-27 14:56:18 +0300

    add topicService.archive tests

--- src/test/groovy/com/github/rodionovsasha/commentservice/services/TopicServiceTest.groovy
+++ src/test/groovy/com/github/rodionovsasha/commentservice/services/TopicServiceTest.groovy
@@ -2,6 +2,7 @@ package com.github.rodionovsasha.commentservice.services
 
 import com.github.rodionovsasha.commentservice.BaseTest
 import com.github.rodionovsasha.commentservice.exceptions.InactiveUserException
+import com.github.rodionovsasha.commentservice.exceptions.TopicAccessException
 import com.github.rodionovsasha.commentservice.exceptions.UserNotFoundException
 import com.github.rodionovsasha.commentservice.repositories.TopicRepository
 import org.springframework.beans.factory.annotation.Autowired
@@ -50,5 +51,59 @@ class TopicServiceTest extends BaseTest {
         then:
         def e = thrown(UserNotFoundException)
         e.message == "The user with id '999' could not be found"
+    }
+
+    def "archive makes a topic archived"() {
+        given:
+        def topicId = 1
+        !topicService.getById(topicId).archived
+
+        when:
+        topicService.archive(topicId, 1)
+
+        then:
+        topicService.getById(topicId).archived
+    }
+
+    def "archive throws when user is inactive"() {
+        given:
+        def topicId = 1
+        !topicService.getById(topicId).archived
+
+        when:
+        topicService.archive(topicId, 2)
+
+        then:
+        !topicService.getById(topicId).archived
+        def e = thrown(InactiveUserException)
+        e.message == "The user with id '2' is not active"
+    }
+
+    def "archive throws when user not found"() {
+        given:
+        def topicId = 1
+        !topicService.getById(topicId).archived
+
+        when:
+        topicService.archive(1, 999)
+
+        then:
+        !topicService.getById(topicId).archived
+        def e = thrown(UserNotFoundException)
+        e.message == "The user with id '999' could not be found"
+    }
+
+    def "archive throws when user is not topic owner"() {
+        given:
+        def topicId = 2
+        !topicService.getById(topicId).archived
+
+        when:
+        topicService.archive(2, 1)
+
+        then:
+        !topicService.getById(topicId).archived
+        def e = thrown(TopicAccessException)
+        e.message == "Non-topic owner with id '1' is trying to archive the topic"
     }
 }
