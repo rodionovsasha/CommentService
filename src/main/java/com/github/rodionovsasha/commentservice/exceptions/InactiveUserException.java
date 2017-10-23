f69c131af7315b9858362814429a72cd4377d7f2
Rodionov Aleksandr <allrod@mail.ru>
2017-10-23 12:40:19 +0300

    added deactivate user

--- src/main/java/com/github/rodionovsasha/commentservice/exceptions/InactiveUserException.java
+++ src/main/java/com/github/rodionovsasha/commentservice/exceptions/InactiveUserException.java
@@ -1,9 +1,5 @@
 package com.github.rodionovsasha.commentservice.exceptions;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.FORBIDDEN)
 public final class InactiveUserException extends ServiceException {
     private InactiveUserException(String message) {
         super(message);
