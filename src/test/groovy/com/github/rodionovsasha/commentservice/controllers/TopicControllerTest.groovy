bca5f72a83fc53cd9f22575485529a187a0e48c4
Rodionov Aleksandr <allrod@mail.ru>
2017-10-25 12:45:51 +0300

    Topic controller - start a new topic (#19)
    * added topic controller get by id
    
    * added topic controller get by id
    
    * fixed exception
    
    * added topic controller - start topic

--- src/test/groovy/com/github/rodionovsasha/commentservice/controllers/TopicControllerTest.groovy
+++ src/test/groovy/com/github/rodionovsasha/commentservice/controllers/TopicControllerTest.groovy
@@ -2,8 +2,11 @@ package com.github.rodionovsasha.commentservice.controllers
 
 import com.github.rodionovsasha.commentservice.entities.Topic
 import com.github.rodionovsasha.commentservice.entities.User
+import com.github.rodionovsasha.commentservice.exceptions.InactiveUserException
 import com.github.rodionovsasha.commentservice.exceptions.TopicNotFoundException
+import com.github.rodionovsasha.commentservice.exceptions.UserNotFoundException
 import com.github.rodionovsasha.commentservice.services.TopicService
+import groovy.json.JsonOutput
 import org.springframework.http.HttpStatus
 import org.springframework.mock.web.MockHttpServletResponse
 import spock.lang.Specification
@@ -12,21 +15,24 @@ import static com.github.rodionovsasha.commentservice.Application.API_BASE_URL
 import static com.github.rodionovsasha.commentservice.controllers.TestUtils.getJsonFromString
 import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup
 import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE
 
 class TopicControllerTest extends Specification {
     final TOPIC_ID = 1
+    final USER_ID = 1
     final NOT_EXISTING_TOPIC_ID = 99
+    final TOPIC_TITLE = "Stupid Flanders"
 
     def service = Mock(TopicService)
     def controller = new TopicController(service)
     def user = Mock(User)
-    def topic = new Topic("Stupid Flanders", user)
+    def topic = new Topic(TOPIC_TITLE, user)
 
     def mockMvc = standaloneSetup(controller).setControllerAdvice(new ExceptionHandlerController()).build()
 
-    def "should get topic by id"() {
+    def "#getById returns topic by id"() {
         when:
         def response = getById(TOPIC_ID)
 
@@ -37,13 +43,13 @@ class TopicControllerTest extends Specification {
             contentType == APPLICATION_JSON_UTF8_VALUE
             def responseJson = getJsonFromString(contentAsString)
             responseJson.id == 0
-            responseJson.title == "Stupid Flanders"
+            responseJson.title == TOPIC_TITLE
             !responseJson.archived
             responseJson.date instanceof Long
         }
     }
 
-    def "should not get topic when not exists"() {
+    def "#getById does not return topic when not exists"() {
         given:
         service.getById(NOT_EXISTING_TOPIC_ID) >> {throw TopicNotFoundException.forId(NOT_EXISTING_TOPIC_ID)}
 
@@ -58,8 +64,75 @@ class TopicControllerTest extends Specification {
         }
     }
 
+    def "#start creates a new topic"() {
+        when:
+        def response = startTopic([title: TOPIC_TITLE], USER_ID)
+
+        then:
+        1 * service.start(TOPIC_TITLE, USER_ID) >> topic
+        with(response) {
+            status == HttpStatus.CREATED.value()
+            contentType == APPLICATION_JSON_UTF8_VALUE
+            def responseJson = getJsonFromString(contentAsString)
+            responseJson.id == 0
+            responseJson.title == TOPIC_TITLE
+            !responseJson.archived
+            responseJson.date instanceof Long
+        }
+    }
+
+    def "#start does not create a new topic when user not exists"() {
+        given:
+        service.start(TOPIC_TITLE, USER_ID) >> {throw UserNotFoundException.forId(USER_ID)}
+
+        when:
+        def response = startTopic([title: TOPIC_TITLE], USER_ID)
+
+        then:
+        with(response) {
+            status == HttpStatus.NOT_FOUND.value()
+            contentType == APPLICATION_JSON_UTF8_VALUE
+            getJsonFromString(contentAsString) == [code: 404, message: "The user with id '1' could not be found"]
+        }
+    }
+
+    def "#start does not create a new topic when user is not active"() {
+        given:
+        service.start(TOPIC_TITLE, USER_ID) >> {throw InactiveUserException.forId(USER_ID)}
+
+        when:
+        def response = startTopic([title: TOPIC_TITLE], USER_ID)
+
+        then:
+        with(response) {
+            status == HttpStatus.FORBIDDEN.value()
+            contentType == APPLICATION_JSON_UTF8_VALUE
+            getJsonFromString(contentAsString) == [code: 403, message: "The user with id '1' is not active"]
+        }
+    }
+
+    def "#start does not create a new topic when title is empty"() {
+        when:
+        def response = startTopic([title: ""], USER_ID)
+
+        then:
+        0 * service.start("", USER_ID)
+        with(response) {
+            status == HttpStatus.INTERNAL_SERVER_ERROR.value()
+            contentType == APPLICATION_JSON_UTF8_VALUE
+            def responseJson = getJsonFromString(contentAsString)
+            responseJson.code == 500
+            responseJson.message.contains("may not be empty")
+        }
+    }
+
     private MockHttpServletResponse getById(long id) {
         mockMvc.perform(get(API_BASE_URL + "/topic/" + id).contentType(APPLICATION_JSON_VALUE))
+                .andReturn().response
+    }
+
+    private MockHttpServletResponse startTopic(Map json, long id) {
+        mockMvc.perform(post(API_BASE_URL + "/topic/user/" + id).contentType(APPLICATION_JSON_VALUE).content(JsonOutput.toJson(json)))
                 .andReturn().response
     }
 }
